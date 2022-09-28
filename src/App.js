80ba6059e4cad3b988003bedfbc5bec6c485a793
Joshua <joshuanivar16@gmail.com>
2022-09-28 16:18:46 -0400

    color-change terminado

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+const ColorSquare = ({ color, ...props }) => {
+  return <div className='color-square' style={{ backgroundColor: color }} {...props} />
+}
+const getRandomNumber = () => Math.round(Math.random() * (255 - 1) + 1);
+const getColor = () => `RGB(${getRandomNumber()},${getRandomNumber()},${getRandomNumber()})`
+
 function App() {
-  const [color, setcolorOne] = useState([]);
+  const [colors, setColors] = useState([]);
   const [colorRandom, setColorRandom] = useState("");
+  const [win, setWin] = useState(false);
+
+  useEffect(() => {
+    generateColors();
+  }, [])
+
+  function generateColors() {
+
+    setWin(false);
+    const colors = new Array(4).fill(0).map(getColor);
 
 
-  function randomColors() {
-    //SE GENERA EL 1R RGB(NUM1,NUM2,NUM3) CON CADA PARAMETRO ALEATORIO
-    const num1 = Math.round(Math.random() * (255 - 1) + 1);
-    const num2 = Math.round(Math.random() * (255 - 1) + 1);
-    const num3 = Math.round(Math.random() * (255 - 1) + 1);
-    //SEGUNDO RGB...
-    const num4 = Math.round(Math.random() * (1 + 255 - 1) + 1);
-    const num5 = Math.round(Math.random() * (1 + 255 - 1) + 1);
-    const num6 = Math.round(Math.random() * (1 + 255 - 1) + 1);
-    const colors1 = `RGB(${num1},${num2},${num3})`
-    const colors2 = `RGB(${num4},${num5},${num6})`
-    const array = [colors1, colors2]// Guradamos los dos RGB en un Array [rgb1, rgb2]
+    setColors(colors); //Actualizamos el estado color con sus dos rgb
 
-    setcolorOne([colors1, colors2]); //Actualizamos el estado color con sus dos rgb
-
-    const ColorRandom = Math.floor(Math.random() * array.length); //escogemos un numero aleatorio del tamaño del array [0,1](0 corresponde a rgb1 y 1 rbg2)
-    const newColorRandom = array[ColorRandom];//escojo el array nuevamente y le doy su nueva posición aleatoria, y lo guardo en una variable nueva
+    const ColorRandom = Math.floor(Math.random() * colors.length); //escogemos un numero aleatorio del tamaño del array [0,1](0 corresponde a rgb1 y 1 rbg2)
+    const newColorRandom = colors[ColorRandom];//escojo el array nuevamente y le doy su nueva posición aleatoria, y lo guardo en una variable nueva
     setColorRandom(newColorRandom);//Actualizo el verdadero color para mostrarlo al usuario y que pueda elegir
-    console.log(newColorRandom);
-    console.log(ColorRandom);
+
 
   }
 
@@ -37,14 +39,11 @@ function App() {
     )
   }
 
-  function divClick(e) {
+  function selectColor(color) {
+    if (color == colorRandom) {
+      setWin(true);
+    }
 
-    /*console.log(e.currentTarget);
-    console.log(e.currentTarget.style.backgroundColor);
-    console.log(color[0]);*/
-    if (color[0] == colorRandom) {
-      console.log("correcto");
-    } else { console.log("acertaste") }
   }
 
   return (
@@ -52,14 +51,17 @@ function App() {
 
       <Label></Label>
       <div className='container'>
-        <div className='colorOne' style={{ backgroundColor: color[0] }} onClick={divClick}></div>
-        <div className='colorTwo' style={{ backgroundColor: color[1] }} onClick={divClick}></div>
+        {
+          colors.map((color) => (
+            < ColorSquare key={color} color={color} onClick={() => selectColor(color)}></ColorSquare>
+          ))
+        }
 
 
       </div>
-      <button onClick={randomColors}>color</button>
+      {win && <button onClick={generateColors}>Reset</button>}
 
-    </div>
+    </div >
 
   );
 }
